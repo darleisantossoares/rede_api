5d529c229ce0ab41397c570b755a3f9ffbb90e7a
darleisantossoares <darleisoares@icloud.com>
2019-06-25 19:22:35 -0300

    cria funcoes de requests

---
+++ rede/exceptions.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+class GetAuthorizedException(Exception):
+    def __init__(self, id, message=None):
+        self.id = id
+        self.message = message
+
+    def __str__(self):
+        return u'%s - %s' % (self.id, self.message)
--- rede/main.py
+++ rede/main.py
@@ -3,3 +3,68 @@ from datetime import datetime
 import os 
 import requests 
 import sys 
+
+class AuthorizationToken:
+    def __init__(self):
+        pass  
+
+    def create(self):
+        pass 
+
+
+class RequestsConciliacao:
+    def __init__(self):
+        pass 
+
+    def consultarVendas(self):
+        pass 
+
+    def consultarParcelas(self):
+        pass 
+
+    def consultarPagamentosSumarizadosCIP(self):
+        pass 
+
+    def consultarpagamentosOrdemCredito(self):
+        pass 
+
+    def consultarRecebiveis(self):
+        pass 
+
+    def consultarRecebiveisSumarizados(self):
+        pass 
+
+    def consultarDebitos(self):
+        pass 
+
+    def consultarDebitosSumarizados(self):
+        pass 
+
+    def consultarListaAjusteDebitos(self):
+        pass 
+
+
+class RequestsCredenciamentos:
+    def __init__(self):
+        pass 
+
+    def createPropostaCredenciamento(self):
+        pass 
+
+    def consultarPropostaCredenciamento(self, idcredenciamento):
+        pass 
+
+    def consultarEstabelecimentoComercial(self):
+        pass 
+
+    def cancelarEstabelcimentoComercial(self):
+        pass 
+
+    def consultarPrecos(self):
+        pass 
+
+    def consultarMCCs(self):
+        pass 
+
+    def realizarLeadCredenciamento(self):
+        pass 
